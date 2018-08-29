6c86fad39dda84a307123a8e8fed4ca8d7e81f82
Lucia Reynoso <rvaicul@gmail.com>
2018-08-29 16:05:48 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+/*
 app.get('/', (req, res) => {
   res.render('home', { msg: 'Hello World!' });
+})
+*/
+
+app.get('/', (req, res) => {
+  res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
