399a0b93969b0b7df4bf10d686b710c4458d6d65
Lucia Reynoso <rvaicul@gmail.com>
2018-08-30 10:44:08 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true }
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
+  rating: Number
 });
 
 /*
@@ -47,11 +48,19 @@ app.post('/reviews', (req, res) => {
     // console.log(req.body);
     Review.create(req.body).then((review) => {
         console.log(review);
-        res.redirect('/');
+        res.redirect(`/reviews/${review._id}`);
     }).catch((err) => {
         console.log(err.message);
     })
 })
+
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+      res.render('reviews-show', {review: review})
+  }).catch((err) => {
+      console.log(err.message);
+  })
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
